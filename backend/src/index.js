096c2ac31f0f08e67abb583c728b3f5b2edbb70f
FischerRobson <fischerrobson@gmail.com>
2020-03-25 20:48:08 -0300

    Back-end

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const routes = require('./routes') // ./ para arquivos
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json()) //  utilizar formato JSON antes das rotas
+app.use(routes)
+
+
+app.listen(3333)
